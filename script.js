42d91428ab323750ddaafd070b75ec4e78bb2fcd
EL-BRUNO <brunosoliveira466@gmail.com>
2023-10-27 19:36:47 -0300

    adição de função que não permite executar o sorteio caso o valor min seja maior que o max

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.ceil(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min)
-  alert(result)
+    if (max > min){
+      const result = Math.floor(Math.random() * (max - min + 1) + min)
+      
+      alert(result)
+    } else {
+      alert('O valor mínimo DEVE ser MENOR  do que o valor máximo!')
+    }
+
+
+    
 }
 
