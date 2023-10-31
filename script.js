851bf7426bbdc5860f2a13973f0098b9ad5887a7
EL-BRUNO <brunosoliveira466@gmail.com>
2023-10-31 11:10:03 -0300

    adição de um input que contém o resultado do sorteio

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function generateNumber(){
+function generateNumber() {
+  const min = Math.ceil(document.querySelector(".input-min").value);
+  const max = Math.ceil(document.querySelector(".input-max").value);
+  const resultElement = document.querySelector(".resultado");
 
-    const min = Math.ceil(document.querySelector(".input-min").value)
-    const max = Math.ceil(document.querySelector(".input-max").value)
+  if (max > min) {
+    const result = Math.floor(Math.random() * (max - min + 1) + min);
 
-    if (max > min){
-      const result = Math.floor(Math.random() * (max - min + 1) + min)
-      
-      alert(result)
-    } else {
-      alert('O valor mínimo DEVE ser MENOR  do que o valor máximo!')
-    }
-
-
-    
+    resultElement.value = result;
+  } else {
+    alert("O valor mínimo DEVE ser MENOR  do que o valor máximo!");
+  }
 }
-
